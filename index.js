ce36f88c23f1c866c44be170be415cd8bc3e3d3a
Ritwik Mukherjee <mritwik369@gmail.com>
2020-07-08 16:51:51 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post("/api/sendResponse", async (req, res) => {
+    let speech =
+        req.body.queryResult &&
+            req.body.queryResult.parameters &&
+            req.body.queryResult.parameters.HSCmarks
+            ? req.body.queryResult.parameters.HSCmarks
+            : "Seems like some problem. Speak again.";
+
+    console.log(speech);
+
+    let speechResponse = {
+        google: {
+            expectUserResponse: true,
+            richResponse: {
+                items: [
+                    {
+                        simpleResponse: {
+                            textToSpeech: speech
+                        }
+                    }
+                ]
+            }
+        }
+    };
+
+    return res.json({
+        payload: speechResponse,
+        //data: speechResponse,
+        fulfillmentText: speech,
+        speech: speech,
+        displayText: speech,
+        source: "webhook-vit"
+    });
+});
+
+app.listen(process.env.PORT || 3000);
+console.log("Service Running");
