d2dfde235e9a3151466071b884ed770253b8ebbd
Ritwik Mukherjee <mritwik369@gmail.com>
2020-07-12 16:02:07 +0530

    added menu driver

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get("/", async (req, res) => {
 app.post("/api/sendResponse", async (req, res) => {
     let speech = "";
 
+    let menuItem = req.body.queryResult &&
+        req.body.queryResult.parameters &&
+        req.body.queryResult.parameters.menuItems
+        ? req.body.queryResult.parameters.menuItems
+        : null;
+
     let marks =
         req.body.queryResult &&
             req.body.queryResult.parameters &&
@@ -35,12 +41,12 @@ app.post("/api/sendResponse", async (req, res) => {
         ? req.body.queryResult.parameters.caste
         : "Seems like some problem. Speak again.";
 
-    console.log({ caste, marks });
-
-    if (CET > 0) {
-        if (caste == "open") {
-            if (marks >= 150) {
-                speech = `Thank you for the information !
+    if (menuItem == null) {
+        console.log({ caste, marks });
+        if (CET > 0) {
+            if (caste == "open") {
+                if (marks >= 150) {
+                    speech = `Thank you for the information !
 
                 Reply with a number to get the infromation:
                 
@@ -60,11 +66,11 @@ app.post("/api/sendResponse", async (req, res) => {
                 7. Fees Related Information
                 
                 8. Admission related information`
-            } else {
-                speech = "Sorry, you are ineligible for admission here, please try BSC IT"
-            }
-        } else if (marks >= 145) {
-            speech = `Thank you for the information !
+                } else {
+                    speech = "Sorry, you are ineligible for admission here, please try BSC IT"
+                }
+            } else if (marks >= 145) {
+                speech = `Thank you for the information !
 
             Reply with a number to get the infromation:
             
@@ -84,11 +90,41 @@ app.post("/api/sendResponse", async (req, res) => {
             7. Fees Related Information
             
             8. Admission related information`
+            } else {
+                speech = "Sorry, you are ineligible for admission here, please try BSC IT"
+            }
         } else {
             speech = "Sorry, you are ineligible for admission here, please try BSC IT"
         }
     } else {
-        speech = "Sorry, you are ineligible for admission here, please try BSC IT"
+        switch (menuItem) {
+            case 1:
+                speech = "Form cost and where it can be purchased";
+                break;
+            case 2:
+                speech = "Previous year cutoff";
+                break;
+            case 3:
+                speech = "Placements at VIT";
+                break;
+            case 4:
+                speech = "Faculty at VIT";
+                break;
+            case 5:
+                speech = "Document Required for submission";
+                break;
+            case 6:
+                speech = "Deadline to submit pending documents";
+                break;
+            case 7:
+                speech = "Fees Related Information";
+                break;
+            case 8:
+                speech = "Admission related information";
+                break;
+            default:
+                speech = "Sorry! I didn't get that!"
+        }
     }
 
     console.log(speech);
